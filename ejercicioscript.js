78c392179dee5e1ceb4a5517887caa94eb5341b6
IsaacOrtga <ortga.isaac@gmail.com>
2022-04-22 13:26:16 +0200

    ejercicio frutas

---
+++ ejercicioscript.js
@@ -0,0 +1,15 @@
+let frutas = ["Manzana", "Banana"];
+console.log(frutas);
+
+function añadir(){
+    alert(frutas);
+    let add = frutas.push ('Naranja');
+    alert(add);
+}
+
+
+
+function borrar(){
+    ultimo = frutas.pop();
+    alert(frutas);
+}
